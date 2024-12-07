8b0bd018c8aa3c24c0c1ee1bdc5b6259c9dcd1d3
Facundo Caamaño <facundoadriancaamano@outlook.com>
2024-12-07 15:58:06 -0300

    Configuracion inicial, navbar, login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
